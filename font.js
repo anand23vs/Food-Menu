898588ed57d71373cca706b0ed3b1abf0af898e7
anand23vs <132248672+anand23vs@users.noreply.github.com>
2024-09-27 07:52:51 +0530

    Add files via upload

---
+++ font.js
@@ -0,0 +1,9 @@
+
+const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Item added to cart!');
+    });
+});
